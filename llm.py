4e2388ed07dedd75df372259650666fffa31f6e4
Nirav Patel <nirav4273@gmail.com>
2026-09-15 10:31:58 +0530

    fixed modal key issuie

--- llm.py
+++ llm.py
@@ -19,12 +19,12 @@ if not openai_api_key:
 def load_llm(provider: str):
     if provider == 'openai':
         return ChatOpenAI(
-            model_name=os.getenv('OPENAI_MODEL', '')
+            model=os.getenv('OPENAI_MODEL', '')
         )
     return ChatGroq(
         api_key=groq_api_key,
-        model_name=os.getenv('GROQ_MODEL', '')
+        model=os.getenv('GROQ_MODEL', '')
     )
 
 def init_llm():
-    return load_llm(os.getenv('MODEL_PROVIDER'))
+    return load_llm(os.getenv('MODEL_PROVIDER', ''))
